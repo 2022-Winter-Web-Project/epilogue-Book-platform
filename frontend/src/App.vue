17dcbfe2ea7b7e1ed47f8a8c2ee3f396ca901076
jiuen <0scarl1216@mju.ac.kr>
2022-01-19 01:08:15 +0900

    modify loginpage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <router-view></router-view>
+    <login-page></login-page>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Login from "./components/login.vue";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld,
-  },
+    'login-page':Login
+  }
 };
 </script>
 
