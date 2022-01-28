8e23be3fd5b5895a212ba5a81cd14318e543cafa
HyunSBong <sanseng@mju.ac.kr>
2022-01-28 21:39:22 +0900

    카카오 로그인 오류 수정 & 로그인 response 변경

--- backend/passport/kakaoStrategy.js
+++ backend/passport/kakaoStrategy.js
@@ -1,35 +1,42 @@
 const passport = require("passport");
 const KakaoStrategy = require("passport-kakao").Strategy;
+const bcrypt = require("bcrypt");
 
 const User = require("../models/user");
 
 module.exports = () => {
-    passport.use(
-        new KakaoStrategy({
-                clientID: process.env.KAKAO_REST_API,
-                callbackURL: "/auth/kakao/callback",
-            },
-            async(accessToken, refreshToken, profile, done) => {
-                console.log("kakao profile", profile);
-                try {
-                    const exUser = await User.findOne({
-                        where: { snsId_kakao: profile.id, provider: "kakao" },
-                    });
-                    if (exUser) {
-                        done(null, exUser);
-                    } else {
-                        const newUser = await User.create({
-                            email: profile._json && profile._json.kakao_account_email,
-                            snsId_kakao: profile.id,
-                            provider: "kakao",
-                        });
-                        done(null, newUser);
-                    }
-                } catch (error) {
-                    console.error(error);
-                    done(error);
-                }
-            }
-        )
-    );
+  passport.use(
+    new KakaoStrategy(
+      {
+        clientID: process.env.KAKAO_REST_API,
+        callbackURL: "/auth/kakao/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        console.log("kakao profile", profile);
+        try {
+          const exUser = await User.findOne({
+            where: { snsId_kakao: profile.id, provider: "kakao" },
+          });
+          if (exUser) {
+            done(null, exUser);
+          } else {
+            const randomPassword = Math.random().toString(36).slice(2);
+            const randomContact = Math.random().toString(36).slice(2);
+            const encryptedPW = await bcrypt.hash(randomPassword, 10);
+            const newUser = await User.create({
+              email: profile._json.kakao_account.email,
+              snsId_kakao: profile.id,
+              provider: "kakao",
+              contact: 00011112222,
+              password: encryptedPW,
+            });
+            done(null, newUser);
+          }
+        } catch (error) {
+          console.error(error);
+          done(error);
+        }
+      }
+    )
+  );
 };
--- backend/utils/redis.js
+++ backend/utils/redis.js
@@ -1,4 +1,4 @@
-const redis = require("redis");
-const redisClient = redis.createClient(process.env.REDIS_PORT);
+// const redis = require("redis");
+// const redisClient = redis.createClient(process.env.REDIS_PORT);
 
-module.exports = redisClient;
+// module.exports = redisClient;
