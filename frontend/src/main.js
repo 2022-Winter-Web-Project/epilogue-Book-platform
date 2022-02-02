49d50cfb7af49b8394e5458bb8249250a992a727
HyunSBong <sanseng@mju.ac.kr>
2022-02-03 04:03:08 +0900

    글쓰기 페이지

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,32 +10,35 @@ Vue.prototype.$http = axios;
 Vue.use(VueRouter);
 Vue.use(axios);
 // Vue.use(VueAxios);
-import { MainPage, LoginPage, RegisterPage, DetailPage} from "./pages";
+import { MainPage, LoginPage, RegisterPage, DetailPage, PostUploadPage } from "./pages";
 
-const routes = [
-  {
-    path: "/",
-    component: MainPage,
-  },
-  {
-    path: "/login",
-    component: LoginPage,
-  },
-  {
-    path: "/register",
-    component: RegisterPage,
-  },
-  {
-    path: "/detail",
-    component: DetailPage,
-  }
+const routes = [{
+        path: "/",
+        component: MainPage,
+    },
+    {
+        path: "/login",
+        component: LoginPage,
+    },
+    {
+        path: "/register",
+        component: RegisterPage,
+    },
+    {
+        path: "/detail",
+        component: DetailPage,
+    },
+    {
+        path: "/postUpload",
+        component: PostUploadPage,
+    }
 ];
 
 const router = new VueRouter({
-  routes,
+    routes,
 });
 
 new Vue({
-  render: (h) => h(App),
-  router,
+    render: (h) => h(App),
+    router,
 }).$mount("#app");
---
+++ frontend/src/pages/PostUploadPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header></Header>
+    <PostUpload></PostUpload>
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import PostUpload from "../components/PostUpload.vue";
+
+export default {
+  components: {
+    Header,
+    PostUpload,
+  },
+};
+</script>
+
+<style></style>
